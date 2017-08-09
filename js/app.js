b6e53428dba09c5882db12d2a82f2c27c074cd03
Riccardo Biscaro <Zendrow@users.noreply.github.com>
2017-08-09 13:16:44 +0200

    0002 - commit
    Prima parte angularjs
    -directives
    -modules
    -components
    -expressions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-var app = angular.module('store', []);
+(function () {
+    // buon uso e costume
+
+    // variabili
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'robe a cose',
+            canPurchase: true,
+            soldOut: true
+        }, {
+            name: 'Maronne',
+            price: 5.95,
+            description: 'robe a cose per davvero',
+            canPurchase: false,
+            soldOut: true
+        }
+    ];
+    // gem è un json praticamente
+
+    var app = angular.module('store', []);
+
+    // angular richiama agularjs
+    // module è il modulo
+    // 'store' nome modulo
+    // in [] si mettono le dipendenze
+
+    // expressions
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+    // il controller va dentro la nostra applicazione
+    // ricorda il costruttore
+
+})();
+
